739430701811caa31c5fefdbb9f6d5b0dbea1862
Michael Garofalo <mggarofalo@gmail.com>
2024-08-28 22:06:27 -0400

    Added subtotal and total readonly properties

--- Receipts/Presentation/Shared/ViewModels/ReceiptVM.cs
+++ Receipts/Presentation/Shared/ViewModels/ReceiptVM.cs
@@ -8,6 +8,8 @@ public class ReceiptVM
 	public DateOnly Date { get; set; }
 	public decimal TaxAmount { get; set; }
 
+	public decimal Subtotal => Math.Floor(Items.Sum(i => i.TotalAmount) * 100) / 100;
+	public decimal Total => Subtotal + TaxAmount;
 	public required List<TransactionVM> Transactions { get; set; } = [];
 	public required List<ReceiptItemVM> Items { get; set; } = [];
 }
