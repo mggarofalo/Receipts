c7d087e0cc48fb88e4a7446078ce24a145de7c30
Michael Garofalo <mggarofalo@gmail.com>
2024-09-01 14:42:44 -0400

    Added LoggingService tests. I don't think there's a way to test that the configured logger outputs at the correct log levels. From research it looks like I would need to structure a new LoggingConfiguration, at which point I'm not testing the implementation, but rather that Serilog itself operates correctly.

---
+++ Tests/Presentation.API.Tests/Services/LoggingServiceTests.cs
@@ -0,0 +1,26 @@
+using API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Serilog;
+
+namespace Presentation.API.Tests.Services;
+
+public class LoggingServiceTests
+{
+	[Theory]
+	[InlineData("Development")]
+	[InlineData("Production")]
+	public void AddLoggingService_RegistersLoggingService(string environmentName)
+	{
+		WebApplicationBuilder builder = WebApplication.CreateBuilder();
+		builder.Environment.EnvironmentName = environmentName;
+		builder.AddLoggingService();
+		ServiceProvider serviceProvider = builder.Services.BuildServiceProvider();
+
+		Assert.NotNull(Log.Logger);
+		Assert.NotNull(serviceProvider.GetService<ILoggerFactory>());
+		Assert.NotNull(serviceProvider.GetService<ILogger<LoggingServiceTests>>());
+	}
+}
