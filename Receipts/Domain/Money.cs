a83963eb51d5b3466ee5c0a2f2f77fc402ade17b
Michael Garofalo <mggarofalo@gmail.com>
2024-09-01 23:02:35 -0400

    Added math helpers to Money class

--- Receipts/Domain/Money.cs
+++ Receipts/Domain/Money.cs
@@ -2,4 +2,29 @@ using Common;
 
 namespace Domain;
 
-public record Money(decimal Amount, Currency Currency = Currency.USD);
+public record Money(decimal Amount, Currency Currency)
+{
+	public Money(decimal amount) : this(amount, Currency.USD) { }
+
+	public static Money Zero => new(0);
+
+	public static Money operator +(Money a, Money b)
+	{
+		return new Money(a.Amount + b.Amount, a.Currency);
+	}
+
+	public static Money operator -(Money a, Money b)
+	{
+		return new Money(a.Amount - b.Amount, a.Currency);
+	}
+
+	public static Money operator *(Money a, Money b)
+	{
+		return new Money(Math.Round(a.Amount * b.Amount, 2), a.Currency);
+	}
+
+	public static Money operator /(Money a, Money b)
+	{
+		return new Money(Math.Round(a.Amount / b.Amount, 2), a.Currency);
+	}
+}
