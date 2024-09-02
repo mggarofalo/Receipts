796e959f28334a802d16e0379944976eaa649f3b
Michael Garofalo <mggarofalo@gmail.com>
2024-09-02 16:23:38 -0400

    Removed unnecessary rounding operation in money multiplication

--- Receipts/Domain/Money.cs
+++ Receipts/Domain/Money.cs
@@ -20,7 +20,7 @@ public record Money(decimal Amount, Currency Currency)
 
 	public static Money operator *(Money a, Money b)
 	{
-		return new Money(Math.Round(a.Amount * b.Amount, 2), a.Currency);
+		return new Money(a.Amount * b.Amount, a.Currency);
 	}
 
 	public static Money operator /(Money a, Money b)
